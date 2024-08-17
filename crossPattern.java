64b88d003d716a9b1a1b403d0ebb1efed957f44a
sabeel28 <141610619+sabeel28@users.noreply.github.com>
2024-08-17 23:41:41 +0530

    Add files via upload

---
+++ crossPattern.java
@@ -0,0 +1,15 @@
+public class crossPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(i+j==6||j-i==0){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
