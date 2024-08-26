90fdd4cca1bc5303b16980c81222b07105264d51
sabeel28 <141610619+sabeel28@users.noreply.github.com>
2024-08-26 13:36:42 +0530

    operators.java

---
+++ src/operators.java
@@ -0,0 +1,18 @@
+public class operators {
+    public static void main(String[] args){
+        int A=10;
+        int B=0;
+        B=A++;//post-increment
+        System.out.println(B);
+        System.out.println(A);
+//        B=++A;//pre-increment
+//        System.out.println(B);
+//        System.out.println(A);
+//        B=A--;//post-decrement
+//        System.out.println(B);
+//        System.out.println(A);
+//        B=--A;//pre-decrement
+//        System.out.println(B);
+//        System.out.println(A);
+    }
+}
