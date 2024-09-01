fc81c1b2efec5825444add06e2ffae3cc29407f2
Sabeel Aijaz <141610619+sabeel28@users.noreply.github.com>
2024-09-01 17:32:56 +0530

    Create swappingNumber.java

---
+++ src/swappingNumber.java
@@ -0,0 +1,15 @@
+//wap program tp swap two number 
+public class Main
+{
+	public static void main(String[] args) {
+		int x=12;
+		int y=34;
+		int t;
+		t=x;
+		x=y;
+		y=t;
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+}
