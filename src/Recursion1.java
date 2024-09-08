f5ff9ada1608b72f492ef947e83c603541aec837
Sabeel Aijaz <141610619+sabeel28@users.noreply.github.com>
2024-09-08 20:19:42 +0530

    Update Recursion1.java

--- src/Recursion1.java
+++ src/Recursion1.java
@@ -1,9 +1,10 @@
+//print from 5 to 1
 public class Recursion1 {
     public static void printNmb(int n){
         if(n==0) {
             return;
         }
-        System.out.println(n);
+        System.out.println(n);//first we print n i.e. 5 and rest of the work is done by the function itself.
         printNmb(n-1);
     }
     public static void main(String args[]){
@@ -11,3 +12,17 @@ public class Recursion1 {
         printNmb(n);
     }
 }
+//print from 1 to 5
+//public class Recursion1 {
+//    public static void printNumb(int n){
+//        if(n==6){
+//            return;
+//        }
+//        System.out.println(n);
+//        printNumb(n+1);
+//    }
+//    public static void main(String args[]){
+//        int n=1;
+//        printNumb(n);
+//    }
+//}
